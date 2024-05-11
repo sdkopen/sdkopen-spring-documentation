bddc9676311d3ed87e5fda3a863c28372480bb1d
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-05-11 15:14:17 -0300

    feat: init project

---
+++ README.md
@@ -0,0 +1,41 @@
+<img src="https://github.com/SeniorityMeter/spring-sm-starter-bom/assets/36059306/ebfcb364-caea-48eb-972a-2d1ae63f4cdb" alt="logo" width="100"/>
+
+# Seniority Meter
+## Spring Security
+
+### Description
+This library is a simple documentation library for Spring Boot applications. It provides a simple configuration of provides documentation for your applications. 
+
+___
+
+### How to use
+#### 1. Add the following dependency and repository to your `pom.xml` file:
+
+```xml
+<dependencies>
+    <dependency>
+        <groupId>br.com.senioritymeter</groupId>
+        <artifactId>documentation</artifactId>
+        <version>1.0.0</version>
+    </dependency>
+</dependencies>
+```
+___
+
+#### 2. Add the following properties to your `application.yaml` file:
+
+```yaml
+spring:
+  documentation:
+    contact:
+      name: ${DOCUMENTATION_CONTACT_NAME}
+      email: ${DOCUMENTATION_CONTACT_EMAIL}
+    info:
+      title: ${DOCUMENTATION_INFO_TITLE}
+      description: ${DOCUMENTATION_INFO_DESCRIPTION}
+      version: ${DOCUMENTATION_INFO_VERSION}
+      termsOfService: ${DOCUMENTATION_INFO_TERMS_OF_SERVICE}
+    license:
+      name: ${DOCUMENTATION_LICENSE_NAME}
+      url: ${DOCUMENTATION_LICENSE_URL}
+```
---
+++ src/main/java/br/com/senioritymeter/documentation/configuration/OpenApiConfig.java
@@ -0,0 +1,56 @@
+package br.com.senioritymeter.documentation.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+  @Value("${spring.documentation.contact.name}")
+  private String documentationContactName;
+
+  @Value("${spring.documentation.contact.email}")
+  private String documentationContactEmail;
+
+  @Value("${spring.documentation.license.name}")
+  private String documentationLicenseName;
+
+  @Value("${spring.documentation.license.url}")
+  private String documentationLicenseUrl;
+
+  @Value("${spring.documentation.info.title}")
+  private String documentationInfoTitle;
+
+  @Value("${spring.documentation.info.description}")
+  private String documentationInfoDescription;
+
+  @Value("${spring.documentation.info.version}")
+  private String documentationInfoVersion;
+
+  @Value("${spring.documentation.info.termsOfService}")
+  private String documentationInfoTermsOfService;
+
+  @Bean
+  public OpenAPI myOpenAPI() {
+    Contact contact = new Contact();
+    contact.setName(documentationContactName);
+    contact.setEmail(documentationContactEmail);
+
+    License mitLicense = new License().name(documentationLicenseName).url(documentationLicenseUrl);
+
+    Info info =
+        new Info()
+            .title(documentationInfoTitle)
+            .version(documentationInfoVersion)
+            .contact(contact)
+            .description(documentationInfoDescription)
+            .termsOfService(documentationInfoTermsOfService)
+            .license(mitLicense);
+
+    return new OpenAPI().info(info);
+  }
+}
